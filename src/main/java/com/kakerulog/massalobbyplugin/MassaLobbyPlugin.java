028e52477cd7b80f6a0132f510d065c63eaa9bf6
BuildTools <unconfigured@null.spigotmc.org>
2021-04-19 18:38:59 +0900

    first commit

---
+++ src/main/java/com/kakerulog/massalobbyplugin/MassaLobbyPlugin.java
@@ -0,0 +1,21 @@
+package com.kakerulog.massalobbyplugin;
+
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MassaLobbyPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Location loc = new Location(e.getPlayer().getWorld(),0.5,45,-0.5,0,0);
+        e.getPlayer().teleport(loc);
+    }
+}
